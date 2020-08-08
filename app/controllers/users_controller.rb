1b6c1cb47a82332bfd88a9615f49143493d5f8a4
Abdul Khan <akhan17@outlook.com>
2020-08-08 22:30:30 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,18 @@ class UsersController < ApplicationController
     def new
       @user = User.new
     end
-  
+
     def create
       @user = User.new(user_params)
       if @user.valid?
         @user.save
-        session[:user_id] = @user.id 
+        session[:user_id] = @user.id
         redirect_to '/'
       else
         redirect_to :signup
       end
     end
-  
+
   private
     def user_params
       params.require(:user).permit(:name, :password, :password_confirmation)
